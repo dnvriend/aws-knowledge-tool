80b759a1ed0a343b43df933c988bb648dfe0c389
Dennis Vriend <dnvriend@gmail.com>
2025-11-20 08:31:50 +0100

    Initial commit from cookiecutter-python-cli-uv

---
+++ aws_knowledge_tool/__init__.py
@@ -0,0 +1,7 @@
+"""aws-knowledge-tool: A CLI that queries the AWS knowledge base
+
+Note: This code was generated with assistance from AI coding tools
+and has been reviewed and tested by a human.
+"""
+
+__version__ = "0.1.0"
